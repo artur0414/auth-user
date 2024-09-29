0e0dc11321fbb5897f85b446fbc755f27549fede
Pedro Arturo Acosta Sánchez <167545086+artur0414@users.noreply.github.com>
2024-09-28 20:04:20 -0500

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from "cors";
 
 const ACCEPTED_ORIGINS = ["http://localhost:3000"];
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
+export const corsMiddleware = ({ acceptedOrigins = ["*"] } = {}) => {
   return cors({
     origin: (origin, callback) => {
       if (acceptedOrigins.includes(origin) || !origin) {
